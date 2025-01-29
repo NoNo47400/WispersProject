4254df5d01bd7492f39b90e30c9485df6475a6f5
Clem <clementlabastidette@gmail.com>
2025-01-29 20:36:13 +0100

    design et opti clock

--- VHDL/MODEM_WISPER/MODEM_WISPER.srcs/sources_1/new/FILTER.vhd
+++ VHDL/MODEM_WISPER/MODEM_WISPER.srcs/sources_1/new/FILTER.vhd
@@ -14,56 +14,29 @@ end FILTER;
 architecture Behavioral of FILTER is
 
     -- Déclaration du type et du tableau des échantillons
-    type sample_array is array (0 to 31) of unsigned(7 downto 0); -- Tableau de 32 échantillons de 8 bits
+    type sample_array is array (0 to 15) of unsigned(7 downto 0); -- Tableau de 32 échantillons de 8 bits
     signal samples : sample_array := (others => (others => '0')); -- Initialisation des échantillons à 0
 
     -- Autres signaux internes
     signal sum     : unsigned(14 downto 0) := (others => '0');   -- Somme des échantillons (15 bits pour éviter le débordement)
     signal filtered_value : unsigned(7 downto 0) := (others => '0'); -- Valeur filtrée (8 bits)
 
+    signal clock_divided : std_logic :='0';
+    signal counter : unsigned(7 downto 0) := (others => '0');
+    
 begin
 
-    FILTERING : process(CLK_inp, RST_inp)
+    FILTERING : process(clock_divided, RST_inp)
     begin
         if RST_inp = '1' then
             -- Réinitialisation des signaux
             samples <= (others => (others => '0'));
             sum <= (others => '0');
             filtered_value <= (others => '0');
-        elsif rising_edge(CLK_inp) then
-            -- Décalage des échantillons
-            samples(31) <= samples(30);
-            samples(30) <= samples(29);
-            samples(29) <= samples(28);
-            samples(28) <= samples(27);
-            samples(27) <= samples(26);
-            samples(26) <= samples(25);
-            samples(25) <= samples(24);
-            samples(24) <= samples(23);
-            samples(23) <= samples(22);
-            samples(22) <= samples(21);
-            samples(21) <= samples(20);
-            samples(20) <= samples(19);
-            samples(19) <= samples(18);
-            samples(18) <= samples(17);
-            samples(17) <= samples(16);
-            samples(16) <= samples(15);
-            samples(15) <= samples(14);
-            samples(14) <= samples(13);
-            samples(13) <= samples(12);
-            samples(12) <= samples(11);
-            samples(11) <= samples(10);
-            samples(10) <= samples(9);
-            samples(9) <= samples(8);
-            samples(8) <= samples(7);
-            samples(7) <= samples(6);
-            samples(6) <= samples(5);
-            samples(5) <= samples(4);
-            samples(4) <= samples(3);
-            samples(3) <= samples(2);
-            samples(2) <= samples(1);
-            samples(1) <= samples(0);
-            samples(0) <= unsigned(SIGNAL_inp);
+        elsif rising_edge(clock_divided) then
+            -- Décalage des échantillons et ajout de l'input actuel
+            samples(0 to 14) <= samples(1 to 15);
+            samples(15) <= unsigned(SIGNAL_inp);
 
             -- Calcul de la somme des 32 échantillons
             sum <= resize(samples(0), 15) + resize(samples(1), 15) + 
@@ -73,18 +46,25 @@ begin
                    resize(samples(8), 15) + resize(samples(9), 15) + 
                    resize(samples(10), 15) + resize(samples(11), 15) + 
                    resize(samples(12), 15) + resize(samples(13), 15) + 
-                   resize(samples(14), 15) + resize(samples(15), 15) + 
-                   resize(samples(16), 15) + resize(samples(17), 15) + 
-                   resize(samples(18), 15) + resize(samples(19), 15) + 
-                   resize(samples(20), 15) + resize(samples(21), 15) + 
-                   resize(samples(22), 15) + resize(samples(23), 15) + 
-                   resize(samples(24), 15) + resize(samples(25), 15) + 
-                   resize(samples(26), 15) + resize(samples(27), 15) + 
-                   resize(samples(28), 15) + resize(samples(29), 15) + 
-                   resize(samples(30), 15) + resize(samples(31), 15);
+                   resize(samples(14), 15) + resize(samples(15), 15) ;
 
             -- Calcul de la moyenne
-            filtered_value <= sum(14 downto 7); -- Division par 32 (32 échantillons)
+            filtered_value <= sum(13 downto 6); -- Division par 32 (32 échantillons)
+        end if;
+    end process;
+    
+    -- PROCESS POUR DIVISER PAR 100 LA CLOCK SYTEME (100MHz -> 1MHz)
+    CLK_DIVISION : process(CLK_inp, RST_inp)
+    begin
+        if(RST_inp = '1') then
+            counter <= (others => '0');
+            clock_divided <= '0';
+        elsif (CLK_inp'event and CLK_inp = '1') then
+            counter <= counter + 1;
+            if (counter = x"0A") then
+                clock_divided <= not clock_divided;
+                counter <= (others => '0');
+            end if;
         end if;
     end process;
 
